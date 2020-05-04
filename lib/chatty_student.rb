004b3945bf3ccd1bf278acca59e003fc0afe5c39
Loren Cipriano <loren25cipriano@gmail.com>
2020-05-04 01:51:58 +0000

    Automatically backed up by Learn

---
+++ lib/chatty_student.rb
@@ -0,0 +1,10 @@
+# ChattyStudent
+#   #hello
+#     returns a greeting phrase plus a long chatty phrase (FAILED - 1)
+#   #raise_hand
+#     returns the phrase 'Pick me!' ten times. (FAILED - 2)
+
+class ChattyStudent < Student
+  def hello 
+  end 
+end 
